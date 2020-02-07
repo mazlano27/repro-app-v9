def0a8781f69fade2046cf6ea4a67a7e40832946
mazlano27 <azlan@otomateme.com>
2020-02-08 00:07:23 +0800

    added angular material to project using schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import {BrowserModule, BrowserTransferStateModule} from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+const material = [
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
     BrowserTransferStateModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...material
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/lazy-loaded-route/lazy-loaded-route.module.ts
+++ src/app/lazy-loaded-route/lazy-loaded-route.module.ts
@@ -1,15 +1,19 @@
 import {NgModule} from '@angular/core';
 import {LazyLoadedRouteRouting} from './lazy-loaded-route.routing';
 import {LazyLoadedRouteComponent} from './lazy-loaded-route.component';
+import {MatCardModule} from '@angular/material/card';
 
+const material = [
+  MatCardModule
+];
 
 @NgModule({
   declarations: [
     LazyLoadedRouteComponent
   ],
   imports: [
-    LazyLoadedRouteRouting
-
+    LazyLoadedRouteRouting,
+    ...material
   ]
 })
 export class LazyLoadedRouteModule {
